096570ad4fbf591e83e8b7f6a812be50c55cd102
Nyi Nyi Lwin <hello.nyinyilwin@gmail.com>
2023-10-14 14:25:05 +0000

    Update docker_cmd.sh

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -1,10 +1,10 @@
 # https://fly.io/docs/rails/cookbooks/deploy/#enabling-swap
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile
-echo 1 > /proc/sys/vm/overcommit_memory
+#fallocate -l 512M /swapfile
+#chmod 0600 /swapfile
+#mkswap /swapfile
+#echo 10 > /proc/sys/vm/swappiness
+#swapon /swapfile
+#echo 1 > /proc/sys/vm/overcommit_memory
 
 # run the application
 /pb/pocketbase serve --https=benchmarks-production.up.railway.app:443
