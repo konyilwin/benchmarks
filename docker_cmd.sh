f2462cff81ba7f1aae7d27f080f77d37ef23389d
Nyi Nyi Lwin <hello.nyinyilwin@gmail.com>
2023-10-14 14:30:12 +0000

    Update docker_cmd.sh

--- docker_cmd.sh
+++ docker_cmd.sh
@@ -7,4 +7,4 @@
 #echo 1 > /proc/sys/vm/overcommit_memory
 
 # run the application
-/pb/pocketbase serve --https=benchmarks-production.up.railway.app:443
+/pb/pocketbase serve benchmarks-production.up.railway.app
